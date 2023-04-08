556d7766aee3394bb9a10ea9490886ebcbcd83ae
Will <avatec2312@gmail.com>
2023-04-07 22:25:25 -0500

    Main Component template

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Main extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div className="main-container">
+                Main Content
+            </div>
+        )
+    }
+}
+
+export default Main
