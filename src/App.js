b11416c39ff33050824dfacbc4d578e8bf309bf1
Will <avatec2312@gmail.com>
2023-04-07 22:26:39 -0500

    Render components in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      App Component
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
