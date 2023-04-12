bba19808eb0b9ac8f2b25618f76917d9e8867590
Will <avatec2312@gmail.com>
2023-04-12 10:19:32 -0500

    Adding links to NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import "../styles/header.css";
+import logo from "../styles/assets/logo-no-background.png"
 
 class Header extends Component {
     constructor(props){
@@ -7,8 +9,30 @@ class Header extends Component {
 
     render(){
         return (
-            <div className="header-container">
-                <h1>CV App</h1>
+            <div className="wrapper">
+                <div className="header-container">
+                    <div className="aviso">
+                        <p>¡Comunicate con nosotros! +51 991586055</p>
+                    </div>
+                    <header className="main-header">
+                        <img alt="Details-Love-Logo" id="logo" src={logo}/>
+                        <button id="hamburguer-button">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </button>
+                    </header>
+                    <div className="nav-wrapper">
+                        <nav>
+                            <a href="#">OFERTAS</a>
+                            <a href="#">DESAYUNOS SORPRESA</a>
+                            <a href="#">ARREGLOS</a>
+                            <a href="#">FORMAL</a>
+                            <a href="#">OCASIÓN</a>
+                            <a href="#">REGALOS</a>
+                        </nav>
+                    </div>
+                    </div>
             </div>
         )
     }
