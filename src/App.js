2632347ed5a2cddb259c1842cab892cc8259355b
Will <avatec2312@gmail.com>
2023-04-13 08:55:29 -0500

    Creating sidebar menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import "./styles/app.css"
+import "./styles/query/media.css"
 
 function App() {
   return (
---
+++ src/components/Header/SideMenu.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import "../../styles/header/sidebar.css"
+
+
+class SideMenu extends Component {
+    
+    constructor(){
+        super()
+        this.closeNav = this.closeNav.bind(this)
+    }
+
+    closeNav(){
+        document.getElementById("mySidenav").style.width = "0";
+    }
+
+    render(){
+        return (
+            <div id="mySidenav" className="sidenav">
+                <a href="#" className="closebtn" onClick={this.closeNav}>&times;</a>
+                <a href="#">Quienes somos</a>
+                <a href="#">Servicios</a>
+                <a href="#">Clientes</a>
+                <a href="#">Contactos</a>
+            </div>
+        )
+    }
+}
+
+
+export default SideMenu
