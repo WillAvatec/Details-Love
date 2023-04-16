53c52c74e56271688d61e6041ba89ee80ab83b19
Will <avatec2312@gmail.com>
2023-04-16 00:43:04 -0500

    Add component slider and render it on Main

---
+++ src/components/Main/slides.js
@@ -0,0 +1,11 @@
+import slide_1 from "../../styles/slider/slide-1.jpg";
+import slide_2 from "../../styles/slider/slide-2.jpg";
+import slide_3 from "../../styles/slider/slide-3.jpg";
+
+const slides = {
+    slide_1,
+    slide_2,
+    slide_3
+}
+
+export default slides
